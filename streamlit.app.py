a49dbddcf148b459cdf4910dd86a09808a370e0f
Adil nawaz <134604273+Adilnawaz8881212@users.noreply.github.com>
2023-05-30 04:22:15 -0700

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,2 @@
 import streamlit as st
-import seaborn as sns
-
-# Load the "tips" dataset from Seaborn
-df = sns.load_dataset("tips")
-
-# Display the dataframe
-st.dataframe(df)
+st.write("kdjfbwkj")
